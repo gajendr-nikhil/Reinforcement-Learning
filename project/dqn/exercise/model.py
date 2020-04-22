31a688d6b6a803131978a3ca5b3103acd26851cb
Nikhil G <ngajendr@usc.edu>
2020-04-22 01:23:41 -0700

    Deep-RL: Deep-Q Network
    - implemented Deep Q-Network agent with
      OpenAI Gym's LunarLander-v2 environment

--- project/dqn/exercise/model.py
+++ project/dqn/exercise/model.py
@@ -16,7 +16,12 @@ class QNetwork(nn.Module):
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
         "*** YOUR CODE HERE ***"
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
 
     def forward(self, state):
         """Build a network that maps state -> action values."""
-        pass
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
